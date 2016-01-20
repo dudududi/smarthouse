1a0da2d45aed28646915137133d1ef91fd0e798c
Szymon <furmanszymon@gmail.com>
2016-01-20 21:24:03 +0100

    Add authentication

--- app/src/main/java/com/example/drone/smarthouse/LoginActivity.java
+++ app/src/main/java/com/example/drone/smarthouse/LoginActivity.java
@@ -1,76 +1,79 @@
 package com.example.drone.smarthouse;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class LoginActivity extends Activity {
+
+    /**
+     * Service to connect with server
+     */
+    private ConnectionService connectionService;
+
+    /**
+     * Fields to get the credential from EditText's
+     */
+    private String login, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        //Przyklad uzycia asynchronicznego wywolania:
-        final Button button = (Button) findViewById(R.id.loginButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                ConnectionService.getInstance().requestRoomList(new ConnectionService.ResponseHandler() {
-                    @Override
-                    public void onResponseReceived(HashMap<String, Integer> response) {
-                        for(Map.Entry<String, Integer> entry : response.entrySet()) {
-                            String key = entry.getKey();
-                            Log.d("TAG", key + ": "+ response.get(key));
-                        }
-                    }
+        connectionService = ConnectionService.getInstance();
 
-                    @Override
-                    public void onError(String msg) {
-                        Log.d("TAG", "Error: " + msg);
-                    }
-                });
+        /**
+         * Checking status of server
+         */
+        connectionService.checkServerAvailable(new ConnectionService.ResponseHandler() {
+            @Override
+            public void onResponseReceived(HashMap<String, Integer> response) {
+                final EditText loginLabel = (EditText) findViewById(R.id.loginTest);
+                final EditText passwdLabel = (EditText) findViewById(R.id.editText);
 
-                //jak wywolujemy jakis setter, to nie musimy sie martwic o odpowiedz - Logi zapisza się na serwerze
-                ConnectionService.getInstance().setTemperature(30, 1);
+                final Button button = (Button) findViewById(R.id.loginButton);
+                button.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
 
-                //autentykacja do logowania
-                ConnectionService.getInstance().authenticate("ryanchenkie@gmail.com", "secret", new ConnectionService.ResponseHandler() {
-                    @Override
-                    public void onResponseReceived(HashMap<String, Integer> response) {
-                        Log.d("TAG", "User authenticated.");
-                    }
+                        login = loginLabel.getText().toString();
+                        password = passwdLabel.getText().toString();
 
-                    @Override
-                    public void onError(String msg) {
-                        Log.d("TAG", "Error: " + msg);
-                    }
-                });
+                        /**
+                         * Authentication with server
+                         */
+                        connectionService.authenticate(login, password, new ConnectionService.ResponseHandler() {
+                            @Override
+                            public void onResponseReceived(HashMap<String, Integer> response) {
+                                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                startActivity(intent);
+                            }
 
-                //sprawdzanie dostępności serwera
-                ConnectionService.getInstance().checkServerAvailable(new ConnectionService.ResponseHandler() {
-                    @Override
-                    public void onResponseReceived(HashMap<String, Integer> response) {
-                        Log.d("TAG", "Server available");
-                    }
-
-                    @Override
-                    public void onError(String msg) {
-                        Log.d("TAG", "Error: " + msg);
+                            @Override
+                            public void onError(String msg) {
+                                Log.d("TAG", "Error: " + msg);
+                            }
+                        });
                     }
                 });
             }
-        });
 
+            @Override
+            public void onError(String msg) {
+
+            }
+        });
     }
 
-    @Override
+
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
