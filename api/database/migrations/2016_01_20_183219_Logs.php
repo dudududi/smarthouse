c46b1471f6993b38c645a6686f725c4e0990dfe1
Maciej Ciach <mciach@student.agh.edu.pl>
2016-01-20 19:34:49 +0100

    Created Logs migration and table attributes for storing logs

---
+++ api/database/migrations/2016_01_20_183219_Logs.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Logs extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+
+
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('from');
+            $table->string('action');
+            $table->string('value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('logs');
+    }
+}
